414b3f5bb9bebebcb2289d865c1fc368f5b143ab
sedhurabe <sedhurabe@gmail.com>
2023-12-19 17:54:46 +0530

    sidebar logout done and login error message changed

--- src/pages/layout/sidebar/index.jsx
+++ src/pages/layout/sidebar/index.jsx
@@ -2,8 +2,14 @@ import { Link } from 'react-router-dom';
 import './style.scss';
 
 const SideBar = () => {
+    const handleLogout = () => {
+        localStorage.clear();
+        window.location.href = '/';
+    }
+    
     return<aside className="aside">
         <Link className='logo' to='/home'>&#9752;</Link>
+        <div className='nav-links' onClick={() => handleLogout()}>&#558;</div>
     </aside>
 }
 
